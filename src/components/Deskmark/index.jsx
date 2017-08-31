e6ae0214f96adc69bb2f751c04b737a7052cbae7
张三丰 <zhangyz@gveoe.cn>
2017-08-31 15:30:33 +0800

    新建一个项目，方便练习

---
+++ src/components/Deskmark/index.jsx
@@ -0,0 +1,15 @@
+/**
+ * Created by Administrator on 2017/8/25 0025.
+ */
+
+import React from 'react';
+
+class Index extends React.Component {
+    render() {
+        return (
+            <div>Deskmark</div>
+        );
+    }
+}
+
+export default Index;
