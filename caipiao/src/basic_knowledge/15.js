6c925d7270412387b877ddc680107420d6ab0e5e
张三丰 <123456@qq.com>
2018-12-15 14:49:10 +0800

    16、Iterator

--- caipiao/src/basic_knowledge/15.js
+++ caipiao/src/basic_knowledge/15.js
@@ -64,72 +64,71 @@ console.log('异步编程的解决方案');
             }
         });
     };
-    ajax(2).then(()=> {  //小于5就报错
+    ajax(12).then(()=> {  //小于5就报错
         console.log(88888);
     }).catch((err) => {
         console.log(err);
     });
 }
 
-{
-    // 所有的图片加载完后，再加载到页面
-    function loadImg(src) {
-        return new Promise((resolve, reject)=> {
-            let img = document.createElement('img');
-            img.src = src;
-            img.onload = function () {
-                resolve(img);
-            };
-            img.onerror = function (err) {
-                reject(err);
-            };
-        });
-    }
-
-    function showImgs(imgs) {
-        imgs.forEach((img)=> {
-            document.body.appendChild(img);
-        })
-    }
-
-    Promise.all([  //把多个Promise实例，当作一个Promise实例
-        loadImg('http://pic1.win4000.com/pic/9/a3/7e591391054.jpg'),
-        loadImg('http://pic1.win4000.com/pic/9/a3/7e591391055.jpg'),
-        loadImg('http://pic1.win4000.com/pic/9/a3/7e591391056.jpg'),
-        loadImg('http://pic1.win4000.com/pic/9/a3/7e591391056.jpg'),
-        loadImg('http://pic1.win4000.com/pic/9/a3/7e591391058.jpg'),
-        loadImg('http://pic1.win4000.com/pic/9/a3/7e591391059.jpg')
-    ]).then(showImgs);
-}
-
-{
-    // 有一个图片加载完，就显示到页面
-    function loadImg11(src) {
-        return new Promise((resolve, reject)=> {
-            let img = document.createElement('img');
-            img.src = src;
-            img.onload = function () {
-                resolve(img);
-            };
-            img.onerror = function (err) {
-                reject(err);
-            };
-        });
-    }
-
-    function showImgs11(img) {
-        let p = document.createElement('p');
-        p.appendChild(img);
-        document.body.appendChild(p);
-    }
-
-    Promise.race([
-        loadImg11('http://pic1.win4000.com/pic/9/a3/7e591391054.jpg'),
-        loadImg11('http://pic1.win4000.com/pic/9/a3/7e591391055.jpg'),
-        loadImg11('http://pic1.win4000.com/pic/9/a3/7e591391056.jpg'),
-        loadImg11('http://pic1.win4000.com/pic/9/a3/7e591391056.jpg'),
-        loadImg11('http://pic1.win4000.com/pic/9/a3/7e591391058.jpg'),
-        loadImg11('http://pic1.win4000.com/pic/9/a3/7e591391059.jpg')
-    ]).then(showImgs11);
-}
-
+// {
+//     // 所有的图片加载完后，再加载到页面
+//     function loadImg(src) {
+//         return new Promise((resolve, reject)=> {
+//             let img = document.createElement('img');
+//             img.src = src;
+//             img.onload = function () {
+//                 resolve(img);
+//             };
+//             img.onerror = function (err) {
+//                 reject(err);
+//             };
+//         });
+//     }
+//
+//     function showImgs(imgs) {
+//         imgs.forEach((img)=> {
+//             document.body.appendChild(img);
+//         })
+//     }
+//
+//     Promise.all([  //把多个Promise实例，当作一个Promise实例
+//         loadImg('http://pic1.win4000.com/pic/9/a3/7e591391054.jpg'),
+//         loadImg('http://pic1.win4000.com/pic/9/a3/7e591391055.jpg'),
+//         loadImg('http://pic1.win4000.com/pic/9/a3/7e591391056.jpg'),
+//         loadImg('http://pic1.win4000.com/pic/9/a3/7e591391056.jpg'),
+//         loadImg('http://pic1.win4000.com/pic/9/a3/7e591391058.jpg'),
+//         loadImg('http://pic1.win4000.com/pic/9/a3/7e591391059.jpg')
+//     ]).then(showImgs);
+// }
+//
+// {
+//     // 有一个图片加载完，就显示到页面
+//     function loadImg11(src) {
+//         return new Promise((resolve, reject)=> {
+//             let img = document.createElement('img');
+//             img.src = src;
+//             img.onload = function () {
+//                 resolve(img);
+//             };
+//             img.onerror = function (err) {
+//                 reject(err);
+//             };
+//         });
+//     }
+//
+//     function showImgs11(img) {
+//         let p = document.createElement('p');
+//         p.appendChild(img);
+//         document.body.appendChild(p);
+//     }
+//
+//     Promise.race([
+//         loadImg11('http://pic1.win4000.com/pic/9/a3/7e591391054.jpg'),
+//         loadImg11('http://pic1.win4000.com/pic/9/a3/7e591391055.jpg'),
+//         loadImg11('http://pic1.win4000.com/pic/9/a3/7e591391056.jpg'),
+//         loadImg11('http://pic1.win4000.com/pic/9/a3/7e591391056.jpg'),
+//         loadImg11('http://pic1.win4000.com/pic/9/a3/7e591391058.jpg'),
+//         loadImg11('http://pic1.win4000.com/pic/9/a3/7e591391059.jpg')
+//     ]).then(showImgs11);
+// }
--- caipiao/src/basic_knowledge/16.js
+++ caipiao/src/basic_knowledge/16.js
@@ -2,5 +2,44 @@
  * Created by zhangsanfeng on 2018/12/13.
  */
 
-console.log('--------------------------10、set-map数据结构------------------------');
+console.log('--------------------------16、Iterator------------------------');
 
+{
+    let arr = ['hello', 'world'];
+    let map = arr[Symbol.iterator]();
+    console.log(map.next());
+    console.log(map.next());
+    console.log(map.next());
+}
+
+{
+    let obj = {
+        start: [1, 3, 2],
+        end: [7, 9, 8],
+        [Symbol.iterator](){
+            let self = this;
+            let index = 0;
+            let arr = self.start.concat(self.end);
+            let len = arr.length;
+            return {
+                next(){
+                    if (index < len) {
+                        return {
+                            value: arr[index++],
+                            done: false
+                        }
+                    } else {
+                        return {
+                            value: arr[index++],
+                            done: true
+                        }
+                    }
+                }
+            }
+        }
+    };
+    for (let key of obj) {
+        console.log(key);
+    }
+    console.log(obj);
+}
