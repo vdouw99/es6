4a4c3b9d42efe3c22a500e465f0ce025dd9ff813
张三丰 <123456@qq.com>
2018-12-16 21:23:07 +0800

    18、Decorator修饰器

--- caipiao/src/basic_knowledge/18.js
+++ caipiao/src/basic_knowledge/18.js
@@ -2,5 +2,17 @@
  * Created by zhangsanfeng on 2018/12/13.
  */
 
-console.log('--------------------------10、set-map数据结构------------------------');
+import {readonly} from 'core-decorators';
 
+console.log('--------------------------18、Decorator修饰器------------------------');
+console.log('“函数”用来修改“类”的行为');
+console.log('学起来有点费劲，先放弃');
+
+{
+    // class Test {
+    //     @readonly
+    //     time11 = '2018-12-12';
+    // }
+    // let test = new Test();
+    // console.log(test.time11);
+}
